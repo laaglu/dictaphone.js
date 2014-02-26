dc92db58c5a7e64c45501b0ac6db8608eb6fb384
Lukas Laag <laaglu@gmail.com>
2014-02-26 14:37:41 +0100

    Added GPLv3 copyleft to js files

--- js/Player.js
+++ js/Player.js
@@ -1,3 +1,20 @@
+/**********************************************
+ * Copyright (C) 2014 Lukas Laag
+ * This file is part of dictaphone.js.
+ * 
+ * dictaphone.js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * dictaphone.js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with dictaphone.js.  If not, see http://www.gnu.org/licenses/
+ **********************************************/
 define(['backbone', 'model/ClipModel', 'Loader', 'Logger', 'AudioEnv'],
   function(Backbone, ClipModel, Loader, logger, env) {
     "use strict";
