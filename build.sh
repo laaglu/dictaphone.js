7026fb653cc54abbf9ff177bacbc83675399d4f0
Lukas Laag <laaglu@gmail.com>
2014-03-15 18:55:53 +0100

    Restructured the app i18n to allow translation using Transifex

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ rm -rf ../dictaphone.js.optimized/js/vendor/require-2.1.6.js
 rm -rf ../dictaphone.js.optimized/.idea
 rm -rf ../dictaphone.js.optimized/.git
 rm -rf ../dictaphone.js.optimized/img/embedded
+rm -rf ../dictaphone.js.optimized/locales/*/manifest.properties
+rm -rf ../dictaphone.js.optimized/.tx
 sed -e "s@<html class='no-js'>@<html manifest='index.appcache' class='no-js'>@g" index.html | sed -e "s@<script data-main='js/main' src='js/vendor/require-2.1.6.js'></script>@<script type='text/javascript' src='js/main.js'></script>@g" > ../dictaphone.js.optimized/index.html
 rm -rf ../dictaphone.js.zip
 DATE=`date`
