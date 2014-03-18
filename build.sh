fbe6f3ea671f37e78cd873613a97c5783e77c345
Lukas Laag <laaglu@gmail.com>
2014-03-18 13:54:24 +0100

    Fix build system manifest.webapp should not contain appcache_path for packaged app

--- build.sh
+++ build.sh
@@ -11,10 +11,10 @@ rm -rf ../dictaphone.js.optimized/.git
 rm -rf ../dictaphone.js.optimized/img/embedded
 rm -rf ../dictaphone.js.optimized/locales/*/manifest.properties
 rm -rf ../dictaphone.js.optimized/.tx
+rm -rf ../dictaphone.js.optimized/.gitignore
 sed -e "s@<html class='no-js'>@<html manifest='index.appcache' class='no-js'>@g" index.html | sed -e "s@<script data-main='js/main' src='js/vendor/require-2.1.6.js'></script>@<script type='text/javascript' src='js/main.js'></script>@g" > ../dictaphone.js.optimized/index.html
 rm -rf ../dictaphone.js.zip
 DATE=`date`
 VERSION=`grep version manifest.webapp | sed -e 's/[^0-9.]*//g'`
 EXPR="s/#.*/#v${VERSION} ${DATE}/g"
 sed -e "${EXPR}" index.appcache > ../dictaphone.js.optimized/index.appcache
-( cd ../dictaphone.js.optimized ; zip -9 -v -r ../dictaphone.js.zip .)
